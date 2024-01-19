2c008e67a857518a66bc54f00fe674d221ee7da4
Hieu <nbhieu140402@gmail.com>
2024-01-19 19:09:18 +0700

    19/1/2024

---
+++ app/src/main/java/com/example/quanlysinhvienlan1/activity/SignUpActivity.kt
@@ -0,0 +1,12 @@
+package com.example.quanlysinhvienlan1.activity
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.quanlysinhvienlan1.R
+
+class SignUpActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_sign_up)
+    }
+}
