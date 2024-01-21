02f3bf5916b7c0143a3cf5ad8a6aefd272dda6bb
Hieu <nbhieu140402@gmail.com>
2024-01-21 18:05:29 +0700

    Thêm ảnh/ wave/ A quên mk

---
+++ app/src/main/java/com/example/quanlysinhvienlan1/activity/ForgetPasswordActivity.kt
@@ -0,0 +1,12 @@
+package com.example.quanlysinhvienlan1.activity
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.quanlysinhvienlan1.R
+
+class ForgetPasswordActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_forget_password)
+    }
+}
