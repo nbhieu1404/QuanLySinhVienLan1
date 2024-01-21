ab7f99143280b264b32dc3516af1f044b5f08ae5
Hieu <nbhieu140402@gmail.com>
2024-01-21 18:02:02 +0700

    Đổi ánh xạ(Tạo hàm mapping)/ intent ĐN

--- app/src/main/java/com/example/quanlysinhvienlan1/activity/SignUpActivity.kt
+++ app/src/main/java/com/example/quanlysinhvienlan1/activity/SignUpActivity.kt
@@ -1,5 +1,6 @@
 package com.example.quanlysinhvienlan1.activity
 
+import android.content.Intent
 import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
@@ -18,80 +19,93 @@ import com.google.firebase.firestore.FirebaseFirestore
 
 class SignUpActivity : AppCompatActivity() {
     // Tạo FireStore
-    // Intent đăng nhập
     // Dialog chính sách
     private val auth: FirebaseAuth = FirebaseAuth.getInstance()
     private val firestore: FirebaseFirestore = FirebaseFirestore.getInstance()
     private val database: FirebaseDatabase = FirebaseDatabase.getInstance()
+    private var edtUsername: EditText? = null
     private var edtEmail: EditText? = null
-//    private var edtPassword = findViewById<EditText>(R.id.edt_Password)
-//    private var edtConfirmPassword = findViewById<EditText>(R.id.edt_ConfirmPassword)
-//    private var cbPolicy = findViewById<CheckBox>(R.id.cb_Policy)
-//    private var txtPolicy = findViewById<TextView>(R.id.txt_Policy)
-//    private var txtAccept = findViewById<TextView>(R.id.txt_accept)
-//    private var btnSignUp = findViewById<Button>(R.id.btn_SignUp)
-//    private var txtSignin = findViewById<TextView>(R.id.txt_Signin)
+    private var edtPassword: EditText? = null
+    private var edtConfirmPassword: EditText? = null
+    private var cbPolicy: CheckBox? = null
+    private var txtAccept: TextView? = null
+    private var txtPolicy: TextView? = null
+    private var btnSignUp: Button? = null
+    private var txtSignIn: TextView? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_sign_up)
-        var edtUsername = findViewById<EditText>(R.id.edt_Username)
-        edtEmail = findViewById(R.id.edt_Email)
-        var edtPassword = findViewById<EditText>(R.id.edt_Password)
-        var edtConfirmPassword = findViewById<EditText>(R.id.edt_ConfirmPassword)
-        var cbPolicy = findViewById<CheckBox>(R.id.cb_Policy)
-        var txtPolicy = findViewById<TextView>(R.id.txt_Policy)
-        var txtAccept = findViewById<TextView>(R.id.txt_accept)
-        var btnSignUp = findViewById<Button>(R.id.btn_SignUp)
-        var txtSignin = findViewById<TextView>(R.id.txt_Signin)
-
+        // Ánh xạ
+        mapping()
         // Bấm nhập email
+        resetErrorBoxEmail()
+        // Bấm Chấp thuận chính sách
+        resetErrorTextPolicy()
+        // Bấm đăng ký
+        clickSignUp()
+        // Bấm intent đăng nhập
+        intentSignIn()
+    }
+
+    // Ánh xạ
+    private fun mapping() {
+        edtUsername = findViewById(R.id.edt_Username)
+        edtEmail = findViewById(R.id.edt_Email)
+        edtPassword = findViewById(R.id.edt_Password)
+        edtConfirmPassword = findViewById(R.id.edt_ConfirmPassword)
+        cbPolicy = findViewById(R.id.cb_Policy)
+        txtPolicy = findViewById(R.id.txt_Policy)
+        txtAccept = findViewById(R.id.txt_accept)
+        btnSignUp = findViewById(R.id.btn_SignUp)
+        txtSignIn = findViewById(R.id.txt_Signin)
+    }
+
+    // Reset màu editText Email
+    private fun resetErrorBoxEmail(){
         edtEmail?.setOnClickListener {
             edtEmail?.setTextColor(Color.BLACK)
         }
+    }
 
-        // Bấm Chấp thuận chính sách
-        cbPolicy.setOnClickListener {
-            txtPolicy.setTextColor(ContextCompat.getColor(this, R.color.BLUE_P_S))
-            txtAccept.setTextColor(Color.BLACK)
-        }
-
-        // Bấm đăng ký
-        btnSignUp.setOnClickListener {
-            var inputUsername = edtUsername.text.toString().trim()
-            var inputEmail = edtEmail?.text.toString().trim()
-            var inputPassword = edtPassword.text.toString().trim()
-            var inputConfirmPassword = edtConfirmPassword.text.toString().trim()
-            var inputPolicy = cbPolicy.isChecked
-            if (inputUsername.isEmpty()) {
-                edtUsername.error = "Vui lòng nhập tên người dùng"
-            }else if (inputEmail.isEmpty()) {
-                edtEmail?.error = "Vui lòng nhập email"
-            } else if (inputPassword.isEmpty()) {
-                edtPassword.error = "Vui lòng nhập mật khẩu"
-            } else if (inputConfirmPassword.isEmpty()) {
-                edtConfirmPassword.error = "Vui lòng nhập lại mật khẩu"
-            } else if (inputPassword != inputConfirmPassword) {
-                edtConfirmPassword.error = "Mật khẩu không trùng khớp"
-            } else if (!inputPolicy) {
-                txtPolicy.setTextColor(Color.RED)
-                txtAccept.setTextColor(Color.RED)
-            } else if (inputUsername.isNotEmpty() && inputEmail.isNotEmpty()
-                    && inputPassword.isNotEmpty () && inputConfirmPassword.isNotEmpty()
-                    && (inputPassword == inputConfirmPassword)
-                    && inputPolicy) {
-                signUp(inputUsername, inputEmail, inputPassword)
-            }
-//            }else{
-//                checkEmailExists(inputEmail){emailExists ->
-//                    if(emailExists) {
-//                        Toast.makeText(this, "Địa chỉ Email đã tồn tại",
-//                                        Toast.LENGTH_SHORT).show()
-//                    }
-//                }
-//            }
+    // Reset màu textView chính sách
+    private fun resetErrorTextPolicy(){
+        cbPolicy?.setOnClickListener {
+            txtPolicy?.setTextColor(ContextCompat.getColor(this, R.color.BLUE_P_S))
+            txtAccept?.setTextColor(Color.BLACK)
         }
     }
 
+    // Bấm đăng ký
+    private fun clickSignUp(){
+        btnSignUp?.setOnClickListener {
+            var inputUsername = edtUsername?.text.toString().trim()
+            var inputEmail = edtEmail?.text.toString().trim()
+            var inputPassword = edtPassword?.text.toString().trim()
+            var inputConfirmPassword = edtConfirmPassword?.text.toString().trim()
+            var inputPolicy = cbPolicy?.isChecked
+
+            if (inputUsername.isNullOrEmpty()) {
+                edtUsername?.error = "Vui lòng nhập tên người dùng"
+            } else if (inputEmail.isNullOrEmpty()) {
+                edtEmail?.error = "Vui lòng nhập email"
+            } else if (inputPassword.isNullOrEmpty()) {
+                edtPassword?.error = "Vui lòng nhập mật khẩu"
+            } else if (inputConfirmPassword.isNullOrEmpty()) {
+                edtConfirmPassword?.error = "Vui lòng nhập lại mật khẩu"
+            } else if (inputPassword != inputConfirmPassword) {
+                edtConfirmPassword?.error = "Mật khẩu không trùng khớp"
+            } else if (inputPolicy != true) {
+                txtPolicy?.setTextColor(Color.RED)
+                txtAccept?.setTextColor(Color.RED)
+            } else if (inputUsername.isNotEmpty() && inputEmail.isNotEmpty()
+                && inputPassword.isNotEmpty () && inputConfirmPassword.isNotEmpty()
+                && (inputPassword == inputConfirmPassword)
+                && inputPolicy) {
+                signUp(inputUsername, inputEmail, inputPassword)
+            }
+        }
+    }
     // Kiểm tra Email đã tồn tại
     private fun checkEmailExists(email: String, callback: (Boolean) -> Unit) {
         auth.fetchSignInMethodsForEmail(email)
@@ -173,4 +187,12 @@ class SignUpActivity : AppCompatActivity() {
             }
     }
 
+    // Intent đăng nhập
+    private fun intentSignIn(){
+        txtSignIn?.setOnClickListener {
+            val intent = Intent(this, SignInActivity::class.java)
+            startActivity(intent)
+            finish()
+        }
+    }
 }
