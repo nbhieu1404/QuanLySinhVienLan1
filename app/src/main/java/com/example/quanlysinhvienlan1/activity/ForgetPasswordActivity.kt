fb523800f8badf0e0401dc0c40364edf2bb0cd8c
Hieu <nbhieu140402@gmail.com>
2024-01-23 20:42:42 +0700

    Thêm ProgressBar

--- app/src/main/java/com/example/quanlysinhvienlan1/activity/ForgetPasswordActivity.kt
+++ app/src/main/java/com/example/quanlysinhvienlan1/activity/ForgetPasswordActivity.kt
@@ -1,21 +1,22 @@
 package com.example.quanlysinhvienlan1.activity
 
 import android.content.Intent
-import android.graphics.Color
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import android.widget.Button
 import android.widget.EditText
+import android.widget.RelativeLayout
 import android.widget.TextView
 import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
 import com.example.quanlysinhvienlan1.R
 import com.google.firebase.auth.FirebaseAuth
-import com.google.firebase.auth.FirebaseAuthUserCollisionException
 
 class ForgetPasswordActivity : AppCompatActivity() {
     private var edtEmail: EditText? = null
     private var btnConfirm: Button? = null
     private var txtSignIn: TextView? = null
+    private var layoutProgressBar: RelativeLayout? = null
     private var auth: FirebaseAuth? = FirebaseAuth.getInstance()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -24,12 +25,8 @@ class ForgetPasswordActivity : AppCompatActivity() {
         mapping()
 
         // Bấm xác nhận
-        clickConfirm()
-
-        // Bấm chuyển sang màn đăng nhập
-        intentSignIn()
+        clickEvent()
     }
-
 
 
     //Ánh xạ
@@ -37,6 +34,12 @@ class ForgetPasswordActivity : AppCompatActivity() {
         edtEmail = findViewById(R.id.edt_Email)
         btnConfirm = findViewById(R.id.btn_Confirm)
         txtSignIn = findViewById(R.id.txt_SignIn)
+        layoutProgressBar = findViewById(R.id.layout_ProgressBar)
+    }
+
+    private fun clickEvent() {
+        clickConfirm()
+        intentSignIn()
     }
 
     // Bấm xác nhận
@@ -46,6 +49,7 @@ class ForgetPasswordActivity : AppCompatActivity() {
             if (inputEmail.isEmpty()) {
                 edtEmail?.error = "Vui lòng nhập email"
             } else {
+                showProgressBar(true)
                 auth?.sendPasswordResetEmail(inputEmail)
                     ?.addOnCompleteListener { task ->
                         if (task.isSuccessful) {
@@ -54,6 +58,7 @@ class ForgetPasswordActivity : AppCompatActivity() {
                                 Toast.LENGTH_SHORT
                             ).show()
                             val intent = Intent(this, SignInActivity::class.java)
+                            showProgressBar(false)
                             startActivity(intent)
                             finish()
                         } else {
@@ -62,16 +67,31 @@ class ForgetPasswordActivity : AppCompatActivity() {
                                 "Gửi Email đặt lại mật khẩu thất bại. Vui lòng kiểm tra lại Email",
                                 Toast.LENGTH_SHORT
                             ).show()
+                            showProgressBar(false)
                         }
                     }
             }
         }
     }
+
     // Intent đăng nhập
-    private fun intentSignIn(){
+    private fun intentSignIn() {
         txtSignIn?.setOnClickListener {
             val intent = Intent(this, SignInActivity::class.java)
             startActivity(intent)
+        }
+    }
+
+    private fun showProgressBar(show: Boolean) {
+        if (show) {
+            layoutProgressBar?.visibility = View.VISIBLE
+            btnConfirm?.isEnabled = false
+            edtEmail?.isEnabled = false
+
+        } else {
+            layoutProgressBar?.visibility = View.GONE
+            edtEmail?.isEnabled = true
+            btnConfirm?.isEnabled = true
         }
     }
 }
