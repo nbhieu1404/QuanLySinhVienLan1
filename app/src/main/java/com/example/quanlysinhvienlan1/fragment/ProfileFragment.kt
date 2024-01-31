9fc9e06507835548676bcb8bb01e126b5feddcb2
Hieu <nbhieu140402@gmail.com>
2024-01-31 21:10:23 +0700

    Đổi tên người dùng, lấy tên người dùng, chuyển f thông tin chi tiết

--- app/src/main/java/com/example/quanlysinhvienlan1/fragment/ProfileFragment.kt
+++ app/src/main/java/com/example/quanlysinhvienlan1/fragment/ProfileFragment.kt
@@ -3,6 +3,7 @@ package com.example.quanlysinhvienlan1.fragment
 import android.app.AlertDialog
 import android.content.Intent
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -13,21 +14,26 @@ import android.widget.RelativeLayout
 import android.widget.TextView
 import android.widget.Toast
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentManager
+import androidx.fragment.app.FragmentTransaction
 import com.example.quanlysinhvienlan1.R
 import com.example.quanlysinhvienlan1.activity.SignInActivity
 import com.google.firebase.auth.EmailAuthProvider
 import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.firestore.FirebaseFirestore
 
 private const val ARG_PARAM1 = "param1"
 private const val ARG_PARAM2 = "param2"
 
 class ProfileFragment : Fragment() {
     private var layoutUser: LinearLayout? = null
+    private var txtUsername: TextView? = null
     private var layoutAccountSettings: LinearLayout? = null
     private var layoutChangeUsername: LinearLayout? = null
     private var layoutChangePassword: LinearLayout? = null
     private var btnLogout: Button? = null
-    private var auth: FirebaseAuth = FirebaseAuth.getInstance()
+    private val auth: FirebaseAuth = FirebaseAuth.getInstance()
+    private val fireStore: FirebaseFirestore = FirebaseFirestore.getInstance()
     private var isSettingsVisible = true
 
     private var param1: String? = null
@@ -48,6 +54,8 @@ class ProfileFragment : Fragment() {
         val view = inflater.inflate(R.layout.fragment_profile, container, false)
         // Ánh xạ
         mapping(view)
+        // Lấy dữ liệu người dùng
+        getUserData()
         // CLick sự kiện
         clickEvent()
         return view
@@ -56,6 +64,7 @@ class ProfileFragment : Fragment() {
     //Ánh xạ
     private fun mapping(view: View) {
         layoutUser = view.findViewById(R.id.layout_User)
+        txtUsername = view.findViewById(R.id.txt_Username)
         layoutAccountSettings = view.findViewById(R.id.layout_AccountSettings)
         layoutChangeUsername = view.findViewById(R.id.layout_ChangeUsername)
         layoutChangePassword = view.findViewById(R.id.layout_ChangePassword)
@@ -64,6 +73,10 @@ class ProfileFragment : Fragment() {
 
     //CLick sự kiện
     private fun clickEvent() {
+        // chuyển sang trang nội dung người dùng
+        layoutUser?.setOnClickListener {
+            navigateToPersonalPageFragment()
+        }
         // Hiển thị/ ẩn settings
         layoutAccountSettings?.setOnClickListener {
             if (isSettingsVisible) {
@@ -79,6 +92,10 @@ class ProfileFragment : Fragment() {
         // Đổi mật khẩu
         layoutChangePassword?.setOnClickListener {
             changePassword()
+        }
+        // Đổi tên người dùng
+        layoutChangeUsername?.setOnClickListener{
+            changeUsername()
         }
     }
 
@@ -126,6 +143,58 @@ class ProfileFragment : Fragment() {
         }
     }
 
+
+    private fun changeUsername(){
+        val builder = AlertDialog.Builder(requireContext())
+        val inflater = requireActivity().layoutInflater
+        val view: View = inflater.inflate(R.layout.custom_dialog_change_username, null)
+        builder.setView(view)
+        val dialog = builder.create()
+
+        var prbChangeUsername = view.findViewById<RelativeLayout>(R.id.prb_ChangeUsername)
+        val edtNewFirstName = view.findViewById<EditText>(R.id.edt_NewFirstName)
+        val edtNewLastName = view.findViewById<EditText>(R.id.edt_NewLastName)
+        val btnConfirmChangeUsername = view.findViewById<Button>(R.id.btn_ConfirmChangeUsername)
+        val btnCancelChangeUsername = view.findViewById<Button>(R.id.btn_CancelChangeUsername)
+
+        val userID = auth.currentUser?.uid
+
+        btnConfirmChangeUsername.setOnClickListener {
+            val inputNewFirstName = edtNewFirstName.text.toString().trim()
+            val inputNewLastName = edtNewLastName.text.toString().trim()
+            val inputFullName = "$inputNewFirstName $inputNewLastName"
+            if(inputNewFirstName.isNullOrEmpty()){
+                edtNewFirstName.error = "Vui lòng nhập họ"
+            }else if(inputNewLastName.isNullOrEmpty()){
+                edtNewLastName.error = "Vui lòng nhập tên"
+            }else if(inputNewFirstName.isNotEmpty() && inputNewLastName.isNotEmpty() && userID!=null) {
+                prbChangeUsername.visibility = View.VISIBLE
+                edtNewFirstName.isEnabled = false
+                edtNewLastName.isEnabled = false
+                btnConfirmChangeUsername.isEnabled = false
+                val useRef = fireStore.collection("users").document(userID)
+                val updateName = hashMapOf<String, Any>(
+                    "username" to inputFullName
+                )
+                useRef.update(updateName)
+                    .addOnSuccessListener {
+                        Toast.makeText(context,"Thay đổi tên thành công", Toast.LENGTH_SHORT).show()
+                        dialog.dismiss()
+                    }
+                    .addOnFailureListener {
+                        Toast.makeText(context,"Thay đổi tên thất bại", Toast.LENGTH_SHORT).show()
+                        prbChangeUsername.visibility = View.GONE
+                        edtNewFirstName.isEnabled = true
+                        edtNewLastName.isEnabled = true
+                        btnConfirmChangeUsername.isEnabled = true
+                    }
+            }
+        }
+        btnCancelChangeUsername.setOnClickListener {
+            dialog.dismiss()
+        }
+        dialog.show()
+    }
     // Đổi mật khẩu
     private fun changePassword() {
         val builder = AlertDialog.Builder(requireContext())
@@ -214,6 +283,37 @@ class ProfileFragment : Fragment() {
             dialog.dismiss()
         }
         dialog.show()
+    }
+
+    // Lấy dữ liệu người dùng
+    private fun getUserData(){
+        if(auth.currentUser!=null){
+            val userID = auth.currentUser!!.uid
+            val userDocRef = fireStore.collection("users").document(userID)
+            userDocRef.get()
+                .addOnSuccessListener { documentSnapshot ->
+                    if(documentSnapshot != null && documentSnapshot.exists()){
+                        val fireStoreUsername = documentSnapshot.getString("username")
+                        txtUsername?.text = fireStoreUsername
+                    }else{
+                        txtUsername?.text = "Loading..."
+                    }
+                }
+                .addOnFailureListener{ exception ->
+                    Log.e("FalseUsernameProfileFragment", "${exception.message}")
+                }
+        }
+
+    }
+
+    // Điều hướng sang trang cá nhân
+    private fun navigateToPersonalPageFragment(){
+        val fragmentPersonalPage = PersonalPageFragment()
+        val framentManager: FragmentManager = requireActivity().supportFragmentManager
+        val transaction: FragmentTransaction = framentManager.beginTransaction()
+        transaction.replace(R.id.fragment_Profile, fragmentPersonalPage)
+        transaction.addToBackStack(null)
+        transaction.commit()
     }
 
     companion object {
