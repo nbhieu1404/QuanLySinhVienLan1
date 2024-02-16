35eeda2fb545c8ca4ac51cc0acf4bc08f179a455
Hieu <nbhieu140402@gmail.com>
2024-02-16 21:04:36 +0700

    Thêm thuộc tính avatar & coverImage

--- app/src/main/java/com/example/quanlysinhvienlan1/data/User.kt
+++ app/src/main/java/com/example/quanlysinhvienlan1/data/User.kt
@@ -2,5 +2,7 @@ package com.example.quanlysinhvienlan1.data
 
 class User (
     var username: String,
-    var email: String
+    var email: String,
+    var avatar: String = "",
+    var coverImage: String = ""
 )
