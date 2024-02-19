a08e89791e537a38e447dc267d6b7bad54509ecb
Hieu <nbhieu140402@gmail.com>
2024-02-19 23:25:14 +0700

    Tối ưu code

--- app/src/main/java/com/example/quanlysinhvienlan1/activity/ForgetPasswordActivity.kt
+++ app/src/main/java/com/example/quanlysinhvienlan1/activity/ForgetPasswordActivity.kt
@@ -13,85 +13,83 @@ import com.example.quanlysinhvienlan1.R
 import com.google.firebase.auth.FirebaseAuth
 
 class ForgetPasswordActivity : AppCompatActivity() {
-    private var edtEmail: EditText? = null
-    private var btnConfirm: Button? = null
-    private var txtSignIn: TextView? = null
-    private var layoutProgressBar: RelativeLayout? = null
-    private var auth: FirebaseAuth? = FirebaseAuth.getInstance()
+    private lateinit var edtEmail: EditText
+    private lateinit var btnConfirm: Button
+    private lateinit var txtSignIn: TextView
+    private lateinit var prbForgetPassword: RelativeLayout
+    private lateinit var auth: FirebaseAuth
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_forget_password)
+        auth = FirebaseAuth.getInstance()
         // Ánh xạ
-        mapping()
+        mappingViews()
 
-        // Bấm xác nhận
-        clickEvent()
+        // Thiết lập sự kiện click
+        setUpClickEvents()
     }
 
-
-    //Ánh xạ
-    private fun mapping() {
+    // Ánh xạ các view
+    private fun mappingViews() {
         edtEmail = findViewById(R.id.edt_Email)
         btnConfirm = findViewById(R.id.btn_Confirm)
         txtSignIn = findViewById(R.id.txt_SignIn)
-        layoutProgressBar = findViewById(R.id.layout_ProgressBar)
+        prbForgetPassword = findViewById(R.id.layout_ProgressBar)
     }
 
-    private fun clickEvent() {
-        clickConfirm()
-        intentSignIn()
-    }
-
-    // Bấm xác nhận
-    private fun clickConfirm() {
-        btnConfirm?.setOnClickListener {
-            val inputEmail = edtEmail?.text.toString().trim()
-            if (inputEmail.isEmpty()) {
-                edtEmail?.error = "Vui lòng nhập email"
-            } else {
-                showProgressBar(true)
-                auth?.sendPasswordResetEmail(inputEmail)
-                    ?.addOnCompleteListener { task ->
-                        if (task.isSuccessful) {
-                            Toast.makeText(
-                                this, "Kiểm tra Email để đặt lại mật khẩu",
-                                Toast.LENGTH_SHORT
-                            ).show()
-                            val intent = Intent(this, SignInActivity::class.java)
-                            showProgressBar(false)
-                            startActivity(intent)
-                            finish()
-                        } else {
-                            Toast.makeText(
-                                this,
-                                "Gửi Email đặt lại mật khẩu thất bại. Vui lòng kiểm tra lại Email",
-                                Toast.LENGTH_SHORT
-                            ).show()
-                            showProgressBar(false)
-                        }
-                    }
-            }
+    // Thiết lập sự kiện click
+    private fun setUpClickEvents() {
+        btnConfirm.setOnClickListener {
+            handleConfirmButtonClick()
+        }
+        txtSignIn.setOnClickListener {
+            navigateToSignInActivity()
         }
     }
 
-    // Intent đăng nhập
-    private fun intentSignIn() {
-        txtSignIn?.setOnClickListener {
-            val intent = Intent(this, SignInActivity::class.java)
-            startActivity(intent)
-        }
-    }
-
-    private fun showProgressBar(show: Boolean) {
-        if (show) {
-            layoutProgressBar?.visibility = View.VISIBLE
-            btnConfirm?.isEnabled = false
-            edtEmail?.isEnabled = false
-
+    // Xử lý sự kiện khi nút xác nhận được nhấn
+    private fun handleConfirmButtonClick() {
+        val inputEmail = edtEmail.text.toString().trim()
+        if (inputEmail.isEmpty()) {
+            edtEmail.error = "Vui lòng nhập email"
         } else {
-            layoutProgressBar?.visibility = View.GONE
-            edtEmail?.isEnabled = true
-            btnConfirm?.isEnabled = true
+            showProgressBar(true)
+            auth.sendPasswordResetEmail(inputEmail)
+                .addOnCompleteListener { task ->
+                    if (task.isSuccessful) {
+                        Toast.makeText(
+                            this, "Kiểm tra Email để đặt lại mật khẩu",
+                            Toast.LENGTH_SHORT
+                        ).show()
+                        val intent = Intent(this, SignInActivity::class.java)
+                        showProgressBar(false)
+                        startActivity(intent)
+                        finish()
+                    } else {
+                        Toast.makeText(
+                            this,
+                            "Gửi Email đặt lại mật khẩu thất bại. Vui lòng kiểm tra lại Email",
+                            Toast.LENGTH_SHORT
+                        ).show()
+                        showProgressBar(false)
+                    }
+                }
         }
+    }
+
+
+    // Chuyển đến màn hình đăng nhập
+    private fun navigateToSignInActivity() {
+        val intent = Intent(this, SignInActivity::class.java)
+        startActivity(intent)
+        finish()
+    }
+
+    // Hiển thị hoặc ẩn thanh tiến trình
+    private fun showProgressBar(show: Boolean) {
+        prbForgetPassword.visibility = if (show) View.VISIBLE else View.GONE
+        btnConfirm.isEnabled = !show
+        edtEmail.isEnabled = !show
     }
 }
