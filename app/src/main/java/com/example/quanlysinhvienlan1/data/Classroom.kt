ad3d6dfe731a4ac8210409a29a3f054190391ed5
Hieu <nbhieu140402@gmail.com>
2024-02-28 00:13:59 +0700

    Thêm các giá trị khởi tạo

--- app/src/main/java/com/example/quanlysinhvienlan1/data/Classroom.kt
+++ app/src/main/java/com/example/quanlysinhvienlan1/data/Classroom.kt
@@ -1,9 +1,9 @@
 package com.example.quanlysinhvienlan1.data
 
 class Classroom(
-    var idClassroom: String,
-    var nameClass: String,
-    var teacher: String,
-    var membersQuantity: Int,
+    var idClassroom: String = "",
+    var nameClass: String = "",
+    var teacher: String = "",
+    var membersQuantity: Int = 0,
     var members: List<User> = emptyList()
 )
